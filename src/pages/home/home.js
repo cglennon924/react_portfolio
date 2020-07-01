8474f3a2a299f5808f8e5bfa8e020974914394be
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-07-01 10:56:14 -0400

    added home page

---
+++ src/pages/home/home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Overlay from "../../components/overlay/overlay.js"
+import "./home.css"
+import ProfilePic from "../../components/profilepic/profile_pic.png"
+
+
+const Home = () => {
+    return (
+        <header className="about-me-background">
+            <Overlay />
+            <div role="main" className="container">
+                <div className="jumtron text-center">
+                <h1 className="jumtron-title">About Me</h1>
+                </div>
+                <div className="container about-me-container">
+                    <img src={ProfilePic} alt="profile pic" title="Christopher Glennon" className="about-me-profile-img"></img>
+                    <p className="universal-font"> I am an aspiring Junior Full Stack Web Developer.I am a hardworking individual who is a quick learner and works well with others! </p>
+                </div>
+            </div>
+        </header>
+
+
+    )
+}
+
+export default Home;
