c8db79844dfc3f02e35052964734f6b670c95c7f
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-07-02 10:12:12 -0400

    tweaked profile picture

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -13,7 +13,7 @@ const Home = () => {
                 <h1 className="jumtron-title">About Me</h1>
                 </div>
                 <div className="container about-me-container">
-                    <img src="./public/images/profile_pic.png" alt="profile pic" title="Christopher Glennon" className="about-me-profile-img"></img>
+                    <img src={ProfilePic} alt="profile pic" title="Christopher Glennon" className="about-me-profile-img"></img>
                     <p className="universal-font"> I am an aspiring Junior Full Stack Web Developer.I am a hardworking individual who is a quick learner and works well with others! </p>
                 </div>
             </div>
