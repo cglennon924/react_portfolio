08288acfec05438b1fdb6f3bf8ac78d27947f4c3
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-08-07 14:01:37 -0400

    new images and font

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom";
 import "./navbar.css"
-import Resume from "../resume/resume.pdf";
+import Resume from "../resume/Resume.pdf";
 
 function Navbar() {
     return(
